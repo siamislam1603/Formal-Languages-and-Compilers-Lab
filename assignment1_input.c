32e8a75fd3ab41f70caacd8f6e0eb8b4048af4db
Your Name <you@example.com>
2021-02-07 16:30:07 +0600

    Input file of Assignment1

---
+++ assignment1_input.c
@@ -0,0 +1,22 @@
+
+      #include<stdio.h                      >
+
+
+
+int     main(){
+        //Single line comment
+
+
+    int a = 10    ,     b      =     5;
+
+    printf    ("Hello        W")     ;
+    /* Multi
+            Line *
+                    /comment
+*/  int c;
+    printf("orld");
+    return
+
+    0;
+}// single
+int abc (){}
