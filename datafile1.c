80f953d62092bba61ffe3f4bceb7af76c2db6b18
Your Name <you@example.com>
2021-02-07 16:14:49 +0600

    Display paranthesis by reading whole program

---
+++ datafile1.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    FILE *p1,*p2;
+    char c;
+
+    p1 = fopen("datafile1.c", "r");
+    p2 = fopen("parentheses.txt","w");
+
+    if(!p1)
+        printf("\nFile can't be opened!");
+    else
+    {
+      	while((c = fgetc(p1)) != EOF)
+      	{
+        	if ((c == '(') || (c == ')'))
+           		fputc(c, p2);
+        }
+    }
+    fclose(p1);
+    fclose(p2);
+
+    p2 = fopen("parentheses.txt","r");
+    while((c=fgetc(p2))!=EOF)
+		printf("%c",c);
+    fclose(p2);
+
+    return 0;
+}
+
